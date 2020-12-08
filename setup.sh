0497a1ef84e18a4464ff6b0e5066e0492f53efe1
Schönemann, Andy <Andy.Schoenemann@debeka.de>
2020-12-08 12:17:43 +0100

    updated

--- setup.sh
+++ setup.sh
@@ -19,13 +19,20 @@ apt-get install -y -q -o Dpkg::Options::="--force-confnew" --no-install-recommen
     "docker-ce=${dockerversion}" "docker-ce-cli=${dockerversion}" containerd.io
 
 # prep docker
+echo "[setup.sh] Stop docker"
 systemctl stop docker.service
 rm -rf /var/lib/docker
+echo "[setup.sh] Mkdir docker"
 mkdir -p /etc/docker
 echo "{ \"data-root\": \"$USER_MOUNTPOINT/docker\", \"hosts\": [ \"unix:///var/run/docker.sock\", \"tcp://127.0.0.1:2375\" ] }" > /etc/docker/daemon.json
 sed -i 's|^ExecStart=/usr/bin/dockerd.*|ExecStart=/usr/bin/dockerd|' /lib/systemd/system/docker.service
+
+echo "[setup.sh] daemon-reload"
 systemctl daemon-reload
 # do not auto-enable docker to start due to temp disk issues
+echo "[setup.sh] disable docker.service"
 systemctl disable docker.service
+echo "[setup.sh] start docker.service"
 systemctl start docker.service
+echo "[setup.sh] status docker.service"
 systemctl status docker.service
