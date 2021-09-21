535af9b9088d20e5ef430a7c10aef613d36024a9
Schönemann, Andy <Andy.Schoenemann@debeka.de>
2021-09-21 10:03:27 +0200

    test docker mount point

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ dockerversion="5:${dockerVersionSub}~3-0~ubuntu-bionic"
 blobxferVersion="1.9.4"
 # @see: batch-shipyard/convoy/version.py [https://github.com/Azure/batch-shipyard/blob/d6da749f9cd678037bd520bc074e40066ea35b56/convoy/version.py]
 shipyardVersion="3.9.1"
-userMountpoint="/mnt"
+#userMountpoint="/mnt"
 
 echo "[setup.sh] install docker"
 apt update
@@ -28,10 +28,11 @@ apt install -y -q -o Dpkg::Options::="--force-confnew" --no-install-recommends \
 # prep docker
 echo "[setup.sh] Stop docker"
 systemctl stop docker.service
-rm -rf /var/lib/docker
+#rm -rf /var/lib/docker
 echo "[setup.sh] Mkdir docker"
 mkdir -p /etc/docker
-echo "{ \"data-root\": \"${userMountpoint}/docker\", \"hosts\": [ \"unix:///var/run/docker.sock\", \"tcp://127.0.0.1:2375\" ] }" > /etc/docker/daemon.json
+#echo "{ \"data-root\": \"${userMountpoint}/docker\", \"hosts\": [ \"unix:///var/run/docker.sock\", \"tcp://127.0.0.1:2375\" ] }" > /etc/docker/daemon.json
+echo "{ \"hosts\": [ \"unix:///var/run/docker.sock\", \"tcp://127.0.0.1:2375\" ] }" > /etc/docker/daemon.json
 sed -i 's|^ExecStart=/usr/bin/dockerd.*|ExecStart=/usr/bin/dockerd|' /lib/systemd/system/docker.service
 
 echo "[setup.sh] daemon-reload"
